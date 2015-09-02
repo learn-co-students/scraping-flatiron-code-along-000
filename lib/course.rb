e55e06099d2368e39d9927809d06098a9451cd70
aeremin <aeremin@maxus.ru>
2015-09-02 16:20:27 +0300

    done

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,17 @@
-
+class Course
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  attr_accessor :title, :schedule, :description
+  def initialize
+    @@all << self unless @@all.include?(self)
+  end	
+end
 
